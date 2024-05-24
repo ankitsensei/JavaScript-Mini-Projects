458ed91f468fa06c5ee41e0690b6a6485b46afe0
ankitsensei <main.ankitbhagat@gmail.com>
2024-05-24 11:41:11 +0530

    button hover and active effect added

--- 9_QR_Code_Generator/app.js
+++ 9_QR_Code_Generator/app.js
@@ -1,9 +1,8 @@
-const imgBox = document.querySelector('imgBox');
-const qrImg = document.querySelector('qrImg');
-const qrText = document.querySelector('qrText');
-
+let qrText = document.getElementById('qrText');
+let imgBox = document.getElementById('imgBox');
+let qrImg = document.getElementById('qrImg');
 function generateQR() {
-    qrImg =
+    qrImg.src =
         'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' +
         qrText.value;
 }
