5cd41f7f9273c8a44c08865c60facd1bade851e5
ankitsensei <main.ankitbhagat@gmail.com>
2024-05-29 00:50:20 +0530

    when you will click to the button, then button will hide itself and container will pop up

---
+++ 14_Popup/app.js
@@ -0,0 +1,7 @@
+const submit = document.getElementById('submit');
+const popup = document.querySelector('.popup');
+
+function btnClicked() {
+    submit.style.display = 'none';
+    popup.style.display = 'flex';
+}
