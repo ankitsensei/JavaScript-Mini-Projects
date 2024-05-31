0fe0e89d59064ebdaac7265f296ae1546f38bec7
ankitsensei <main.ankitbhagat@gmail.com>
2024-05-31 09:18:28 +0530

    light mode icon turning into dark mode, but not returining back

--- 15_light_dark_mode/app.js
+++ 15_light_dark_mode/app.js
@@ -1,5 +1,12 @@
-const darkmode = document.querySelector('.fa-moon');
-
-darkmode.addEventListener('click', () => {
-    document.body.classList.toggle('fa-sun');
+const darkMode = document.querySelector('.fa-moon');
+const lightMode = document.querySelector('fa-sun');
+darkMode.addEventListener('click', (e) => {
+    e.preventDefault();
+    darkMode.classList.remove('fa-moon');
+    darkMode.classList.add('fa-sun');
+});
+lightMode.addEventListener('click', (e) => {
+    e.preventDefault();
+    lightMode.classList.remove('fa-sun');
+    lightMode.classList.add('fa-moon');
 });
