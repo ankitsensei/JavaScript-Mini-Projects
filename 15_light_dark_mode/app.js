82c0aeea3421a5f1380038cc81545dc64d48b2ab
ankitsensei <main.ankitbhagat@gmail.com>
2024-05-31 09:38:58 +0530

    added animation to the toggle

--- 15_light_dark_mode/app.js
+++ 15_light_dark_mode/app.js
@@ -6,11 +6,13 @@ document.addEventListener('DOMContentLoaded', function () {
         if (toggleIcon.classList.contains('fa-moon')) {
             toggleIcon.classList.remove('fa-moon');
             toggleIcon.classList.add('fa-sun');
+            body.classList.remove('light-mode');
             body.classList.add('dark-mode');
         } else {
             toggleIcon.classList.remove('fa-sun');
             toggleIcon.classList.add('fa-moon');
             body.classList.remove('dark-mode');
+            body.classList.add('light-mode');
         }
     });
 });
