8b7fec8423d959938fa921c12905d38ab588f58f
ankitsensei <main.ankitbhagat@gmail.com>
2024-06-02 11:18:23 +0530

    validdateName() func. wrote

--- 16_Validation_form/app.js
+++ 16_Validation_form/app.js
@@ -3,3 +3,17 @@ const phoneError = document.getElementById('phone-error');
 const emailError = document.getElementById('email-error');
 const messageError = document.getElementById('message-error');
 const submitError = document.getElementById('submit-error');
+
+function validateName() {
+    var name = document.getElementById('contact-name').value;
+    if (name.length == 0) {
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+    nameError.innerHTML = 'valid';
+    return true;
+}
