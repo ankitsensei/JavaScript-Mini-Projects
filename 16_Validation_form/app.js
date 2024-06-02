1c55527fd5bee35954703791e23ce91ef1d1ea46
ankitsensei <main.ankitbhagat@gmail.com>
2024-06-02 11:51:15 +0530

    added a tick icon to phone no and styled it via js

--- 16_Validation_form/app.js
+++ 16_Validation_form/app.js
@@ -20,3 +20,24 @@ function validateName() {
     nameError.style.color = 'green';
     return true;
 }
+function validatePhone() {
+    let phone = document.getElementById('contact-phone').value;
+    if (phone.length == 0) {
+        phoneError.innerHTML = 'Phone no is required';
+        phoneError.style.color = 'red';
+        return false;
+    }
+    if (phone.length !== 10) {
+        phoneError.innerHTML = 'Phone no should be 10 digits';
+        phoneError.style.color = 'red';
+        return false;
+    }
+    if (!phone.match(/^[0-9]{10}$/)) {
+        phoneError.innerHTML = 'Only digits please';
+        phoneError.style.color = 'red';
+        return false;
+    }
+    phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    phoneError.style.color = 'green';
+    return true;
+}
