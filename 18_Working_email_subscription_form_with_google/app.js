b3e51ec1cc7b37fe05d8ff516515fe8886e925f5
ankitsensei <main.ankitbhagat@gmail.com>
2024-06-03 23:05:15 +0530

    started new project... save user data MySQL

--- 18_Working_email_subscription_form_with_google/app.js
+++ 18_Working_email_subscription_form_with_google/app.js
@@ -1,10 +1,13 @@
 const scriptURL =
-    'https://script.google.com/macros/s/AKfycbzoOCCCSfNWWkD0tHXXf2arhmqYnkHqF19tQcCX31W1yeBEg05CmTbqF2MvBh7ToGYC/exec';
+    'https://script.google.com/macros/s/AKfycbyvsztGkM3bAGZh77L7wA2IPtpaYrVVWQLIlApKihuTZICmfU3bYKtV3pWWidiOHbk/exec';
 const form = document.forms['submit-to-google-sheet'];
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then((response) => console.log('Success!', response))
+    const formData = new FormData(form);
+    const body = { email: formData.get('email') };
+    fetch(scriptURL, { method: 'POST', body: body })
+        .then((response) => response.text())
+        .then((response) => console.log('success', response))
         .catch((error) => console.error('Error!', error.message));
 });
